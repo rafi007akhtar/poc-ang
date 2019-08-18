d009533a9237ebf344803eb64b73c1384915444e
rafi007akhtar <alimdrafi@gmail.com>
2019-08-18 21:24:08 +0530

    Started making the form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'poc-ang';
+  name: string = "";
+  empid: number;
+  mob: number;
+
+  submits() {
+    let details = [this.name, this.empid, this.mob];
+    console.log(details);
+  }
+
+  ngOnInit() {
+
+  }
 }
